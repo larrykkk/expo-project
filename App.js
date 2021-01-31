7a789c48b5fae502f9e816f7f1221763ca52238d
larrykkk <larry46052005@yahoo.com.tw>
2021-01-31 20:04:08 +0800

    feat: add location and notification and camera feature

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Button, View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import NotificationsPage from "./pages/Notifications";
+import CameraPage from "./pages/Camera";
+import LocationPage from "./pages/Location";
 
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to 1"
+        onPress={() => navigation.navigate("NotificationsPage")}
+      />
+      <Button
+        title="Go to 2"
+        onPress={() => navigation.navigate("CameraPage")}
+      />
+      <Button
+        title="Go to 3"
+        onPress={() => navigation.navigate("LocationPage")}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="NotificationsPage" component={NotificationsPage} />
+        <Stack.Screen name="CameraPage" component={CameraPage} />
+        <Stack.Screen name="LocationPage" component={LocationPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
